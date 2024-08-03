a6c58219e420e8b6be39a51e51472700f505e01f
naveensivakumar14 <ersnaveenkumar@gmail.com>
2024-08-03 09:29:42 +0530

    Perfumy navbar styles updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,13 @@ function Navbar(){
       <div>
         <ul className='Nav'>
           <h2>Perfumy</h2>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Search</a></li>
-          <li><a href="#">Products</a></li>
-          <li><a href="#">About Us</a></li>
+          <div className='Nav_items'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Search</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">About Us</a></li>
+          </div>
+          
         </ul>
       </div>
     )
